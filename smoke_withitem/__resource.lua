bfc6591551171bd23670aac2fbf8723a0581630e
Plocks2020 <106162408+Plocks2020@users.noreply.github.com>
2022-11-24 15:32:02 +0100

    Add files via upload

---
+++ smoke_withitem/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author "Plocks#2019"
+description "Smoking by Plocks https://discord.gg/R8Hsyk9vFE"
+
+server_scripts {
+'server.lua'
+}
+
+client_scripts {
+'client.lua'
+}
+
